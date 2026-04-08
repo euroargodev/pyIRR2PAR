f1504c609fde71593a79fd9e08a1f53ec790cb6c
Guillaume Maze <gmaze@ifremer.fr>
2026-04-08 11:07:46 +0200

    Clean code formatting with Black

--- pyIRR2PAR/IRR2PAR.py
+++ pyIRR2PAR/IRR2PAR.py
@@ -1,6 +1,7 @@
 import numpy as np
-from JTan2025_Ed_380_443_490_555 import JTan_PAR_from_Ed
-from JPitarch_Ed_380_443_490_555 import JPitarch_PAR_from_Ed
+from pyIRR2PAR.JTan2025_Ed_380_443_490_555 import JTan_PAR_from_Ed
+from pyIRR2PAR.JPitarch_Ed_380_443_490_555 import JPitarch_PAR_from_Ed
+
 
 def IRR2PAR(Ed, z):
     """
@@ -26,17 +27,19 @@ def IRR2PAR(Ed, z):
 
     """
     # Call JTan_PAR_from_Ed from JTan_2025 with input in uW/Cm2
-    JTan_PAR, JTan_e = JTan_PAR_from_Ed(np.column_stack((100*Ed, z)))
+    JTan_PAR, JTan_e = JTan_PAR_from_Ed(np.column_stack((100 * Ed, z)))
     JTan_PAR_results = [JTan_PAR, JTan_e]
 
-    ## remove negative values for JPitarch_2025
+    # Remove negative values for JPitarch_2025
     Ed[Ed < 0] = np.nan
 
-    ## Modify null and negative depth values for JPitarch_2025, as it uses a logarithmic transformation of z.
+    # Modify null and negative depth values for JPitarch_2025, as it uses a logarithmic transformation of z.
     z[z <= 0] = 1e-6
 
     # Call JPitarch_PAR_from_Ed from JPitarch_2025
-    JPitarch_PAR, JPitarch_PAR_b, JPitarch_ep50, JPitarch_IQR_ep = JPitarch_PAR_from_Ed(Ed, z)
+    JPitarch_PAR, JPitarch_PAR_b, JPitarch_ep50, JPitarch_IQR_ep = JPitarch_PAR_from_Ed(
+        Ed, z
+    )
 
     # Stack JTan_PAR and JPitarch_PAR into a 2D array
     values = np.vstack((JTan_PAR, JPitarch_PAR))
@@ -47,7 +50,13 @@ def IRR2PAR(Ed, z):
     # uncertainties
     # Convert IQR_ep to absolute uncertainty
     JPitarch_e = JPitarch_PAR * (JPitarch_IQR_ep / 100)
-    JPitarch_PAR_results = [JPitarch_PAR, JPitarch_PAR_b, JPitarch_ep50, JPitarch_IQR_ep, JPitarch_e]
+    JPitarch_PAR_results = [
+        JPitarch_PAR,
+        JPitarch_PAR_b,
+        JPitarch_ep50,
+        JPitarch_IQR_ep,
+        JPitarch_e,
+    ]
 
     # Calculate mean_uncertainty based on NaN conditions
     mean_uncertainty = np.where(
@@ -55,33 +64,15 @@ def IRR2PAR(Ed, z):
         np.where(
             np.isnan(JTan_PAR),  # If JTan_PAR is also NaN
             np.nan,  # mean_uncertainty is NaN
-            JTan_e  # mean_uncertainty is JTan_e
+            JTan_e,  # mean_uncertainty is JTan_e
         ),
         np.where(
             np.isnan(JTan_PAR),  # If JTan_PAR is NaN
             JPitarch_e,  # mean_uncertainty is JPitarch_e
-            #np.sqrt(JTan_e ** 2 + JPitarch_e ** 2) / 2  # If neither is NaN, use  LPU (Low of Propagation of Uncertainties ) method assuming no correlation
-            (JTan_e + JPitarch_e) / 2  # If neither is NaN, use  LPU (Low of Propagation of Uncertainties ) method assuming full correlation
-        )
+            # np.sqrt(JTan_e ** 2 + JPitarch_e ** 2) / 2  # If neither is NaN, use  LPU (Low of Propagation of Uncertainties ) method assuming no correlation
+            (JTan_e + JPitarch_e)
+            / 2,  # If neither is NaN, use  LPU (Low of Propagation of Uncertainties ) method assuming full correlation
+        ),
     )
 
     return JTan_PAR_results, JPitarch_PAR_results, mean_PAR, mean_uncertainty
-
-
-# # #Example
-# # #usage:
-# Ed = np.array([[3.1477e-07, 4.401e-05, 0.00031093, 2.6219e-05],
-#                   [0.0097528, 0.061085, 0.10562, 0.087316],
-#                   [0.20572, 0.52315, 0.54544, 0.16185]])
-# z = np.array([84.8, 14.2, 28.1])
-#
-# # Appel de la fonction IRR2PAR
-# JTan_PAR_results, JPitarch_PAR_results, mean_PAR, mean_uncertainty = IRR2PAR(Ed, z)
-#
-#
-#
-# # Affichage des résultats
-# print("Résultats de JTan_PAR_results :", JTan_PAR_results)
-# print("Résultats de JPitarch_PAR_results :", JPitarch_PAR_results)
-# print("Vecteur moyen (mean_par + PAR) :", mean_PAR)
-# print([mean_PAR,mean_uncertainty])
